be6007e078448ef3d8453e231bf8cb7d7988a503
Marius Haugen <Marius.io@protonmail.com>
2019-06-20 00:58:07 +0200

    First commit

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel felis ac arcu hendrerit imperdiet. Nam tempor, elit nec eleifend tempor, lectus ante vulputate dui, ut iaculis massa sapien eu massa. Morbi imperdiet ligula ac erat finibus elementum. Duis consequat ex sit amet neque vestibulum, id posuere tortor laoreet. Curabitur consequat lorem vel odio vulputate cursus. Aliquam vitae purus et ipsum egestas ornare sed eu ex. Nulla sed condimentum quam, sed vestibulum dui. Sed eget enim sit amet mi hendrerit fermentum sed in risus. Nulla arcu ipsum, ullamcorper a rhoncus et, dapibus et ante. Ut purus eros, malesuada quis viverra eu, convallis sit amet mi. Proin non euismod lacus. Maecenas lacinia volutpat diam, a volutpat sapien accumsan eget. Vestibulum non tellus ut tellus ultrices dictum in nec velit. Proin faucibus vestibulum sem ut semper. Nunc faucibus urna in nisl dignissim varius. Donec ut tempor lacus.', 
+            'author': 'Marius Haugen',
+            'create_date': '04-25-2017'
+        },
+
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel felis ac arcu hendrerit imperdiet. Nam tempor, elit nec eleifend tempor, lectus ante vulputate dui, ut iaculis massa sapien eu massa. Morbi imperdiet ligula ac erat finibus elementum. Duis consequat ex sit amet neque vestibulum, id posuere tortor laoreet. Curabitur consequat lorem vel odio vulputate cursus. Aliquam vitae purus et ipsum egestas ornare sed eu ex. Nulla sed condimentum quam, sed vestibulum dui. Sed eget enim sit amet mi hendrerit fermentum sed in risus. Nulla arcu ipsum, ullamcorper a rhoncus et, dapibus et ante. Ut purus eros, malesuada quis viverra eu, convallis sit amet mi. Proin non euismod lacus. Maecenas lacinia volutpat diam, a volutpat sapien accumsan eget. Vestibulum non tellus ut tellus ultrices dictum in nec velit. Proin faucibus vestibulum sem ut semper. Nunc faucibus urna in nisl dignissim varius. Donec ut tempor lacus.', 
+            'author': 'John Doe',
+            'create_date': '10-25-2017'
+        },
+
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel felis ac arcu hendrerit imperdiet. Nam tempor, elit nec eleifend tempor, lectus ante vulputate dui, ut iaculis massa sapien eu massa. Morbi imperdiet ligula ac erat finibus elementum. Duis consequat ex sit amet neque vestibulum, id posuere tortor laoreet. Curabitur consequat lorem vel odio vulputate cursus. Aliquam vitae purus et ipsum egestas ornare sed eu ex. Nulla sed condimentum quam, sed vestibulum dui. Sed eget enim sit amet mi hendrerit fermentum sed in risus. Nulla arcu ipsum, ullamcorper a rhoncus et, dapibus et ante. Ut purus eros, malesuada quis viverra eu, convallis sit amet mi. Proin non euismod lacus. Maecenas lacinia volutpat diam, a volutpat sapien accumsan eget. Vestibulum non tellus ut tellus ultrices dictum in nec velit. Proin faucibus vestibulum sem ut semper. Nunc faucibus urna in nisl dignissim varius. Donec ut tempor lacus.', 
+            'author': 'CoreyInTheWhiteHouse',
+            'create_date': '12-25-2017'
+        }
+    ]
+    return articles
